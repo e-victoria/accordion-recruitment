7509f76070b0528a857d0ae44cee60743c7144fc
vicki <victoria.eremeeva@clorce.com>
2020-12-24 15:08:04 +0100

    Add accordion and accordion-list component

---
+++ src/app/accordion-list/accordion-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-list',
+  templateUrl: './accordion-list.component.html',
+  styleUrls: ['./accordion-list.component.scss']
+})
+export class AccordionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
