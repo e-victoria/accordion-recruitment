d603e7cb00e286dcc96aa8c245af0f40f54d2da0
vicki <victoria.eremeeva@clorce.com>
2020-12-24 15:07:36 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import {AccordionListComponent} from './accordion-list/accordion-list.component';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const Approutes: Routes = [
+  {
+    path: '',
+    component: AccordionListComponent
+  },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { Approutes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { AccordionListComponent } from './accordion-list/accordion-list.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    AccordionListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(Approutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
